357d461683531f6814e5bbf5d9186b920249f9c0
VitorSoaresGoncalves <soares.vitor@unifesp.br>
2026-08-12 22:10:01 -0300

    Torna caminho do banco configurável via variável de ambiente

--- database/db.js
+++ database/db.js
@@ -1,7 +1,11 @@
 const Database = require('better-sqlite3');
 const path = require('path');
 
-const db = new Database(path.join(__dirname, 'jogos.db'));
+const caminhoBanco = process.env.DATABASE_PATH
+    ? process.env.DATABASE_PATH
+    : path.join(__dirname, 'jogos.db');
+
+const db = new Database(caminhoBanco);
 
 db.exec(`
     CREATE TABLE IF NOT EXISTS usuarios (
